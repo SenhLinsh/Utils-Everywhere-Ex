1be4d5c6068c7d52c04a6a567a9391157fc98625
lin.shenghe <lin.shenghe@imprexion.cn>
2020-03-05 21:39:52 +0800

    ✨ 优化方法

--- uee/src/main/java/com/linsh/lshutils/utils/LinearLayoutUtilsEx.java
+++ uee/src/main/java/com/linsh/lshutils/utils/LinearLayoutUtilsEx.java
@@ -1,7 +1,9 @@
 package com.linsh.lshutils.utils;
 
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.ColorFilter;
+import android.graphics.Paint;
 import android.graphics.PixelFormat;
 import android.graphics.drawable.Drawable;
 import android.widget.LinearLayout;
@@ -22,26 +24,56 @@ public class LinearLayoutUtilsEx {
      * @param width 分割线宽度
      */
     public static void setDivider(LinearLayout linearLayout, int width) {
-        linearLayout.setShowDividers(LinearLayout.SHOW_DIVIDER_MIDDLE);
+        setDivider(linearLayout, width, Color.TRANSPARENT);
+    }
+
+    /**
+     * 设置 LinearLayout 的透明分割线
+     *
+     * @param width 分割线宽度
+     */
+    public static void setDivider(LinearLayout linearLayout, int width, int color) {
+        setDivider(linearLayout, width, color, LinearLayout.SHOW_DIVIDER_MIDDLE);
+    }
+
+    /**
+     * 设置 LinearLayout 的透明分割线
+     *
+     * @param width 分割线宽度
+     */
+    public static void setDivider(LinearLayout linearLayout, int width, int color, int dividerFlag) {
+        linearLayout.setShowDividers(dividerFlag);
         if (linearLayout.getOrientation() == LinearLayout.HORIZONTAL) {
-            linearLayout.setDividerDrawable(new DividerDrawable(width, 0));
+            linearLayout.setDividerDrawable(new DividerDrawable(width, 0, color));
         } else {
-            linearLayout.setDividerDrawable(new DividerDrawable(0, width));
+            linearLayout.setDividerDrawable(new DividerDrawable(0, width, color));
         }
     }
 
     private static class DividerDrawable extends Drawable {
 
-        private int width;
-        private int height;
+        private final int width;
+        private final int height;
+        private final Paint paint;
 
         public DividerDrawable(int width, int height) {
             this.width = width;
             this.height = height;
+            this.paint = null;
+        }
+
+        public DividerDrawable(int width, int height, int color) {
+            this.width = width;
+            this.height = height;
+            this.paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+            this.paint.setColor(color);
         }
 
         @Override
         public void draw(Canvas canvas) {
+            if (paint != null) {
+                canvas.drawRect(getBounds(), paint);
+            }
         }
 
         @Override
