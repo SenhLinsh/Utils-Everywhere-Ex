c00657bc741be0dec6b3cd79d0c200b2cb434182
Sehn Linsh <lyuanxiang0812@126.com>
2021-02-03 17:15:43 +0800

    ✨ 优化工具类

--- uee/src/main/java/com/linsh/lshutils/utils/StringUtilsEx.java
+++ uee/src/main/java/com/linsh/lshutils/utils/StringUtilsEx.java
@@ -10,6 +10,9 @@ package com.linsh.lshutils.utils;
  */
 public class StringUtilsEx {
 
+    /**
+     * 自定义字符的 trim
+     */
     public static String trim(String text, char... trims) {
         int len = text.length();
         int st = 0;
@@ -22,6 +25,9 @@ public class StringUtilsEx {
         return ((st > 0) || (len < text.length())) ? text.substring(st, len) : text;
     }
 
+    /**
+     * 自定义字符的左向 trim
+     */
     public static String trimLeft(String text, char... trims) {
         int len = text.length();
         int st = 0;
@@ -31,6 +37,9 @@ public class StringUtilsEx {
         return st > 0 ? text.substring(st, len) : text;
     }
 
+    /**
+     * 自定义字符的右向 trim
+     */
     public static String trimRight(String text, char... trims) {
         int len = text.length();
         while ((0 < len) && ArrayUtilsEx.indexOf(trims, text.charAt(len - 1)) >= 0) {
@@ -39,6 +48,9 @@ public class StringUtilsEx {
         return len < text.length() ? text.substring(0, len) : text;
     }
 
+    /**
+     * 默认字符 + 自定义字符的 trim
+     */
     public static String trimPlus(String text, char... extras) {
         int len = text.length();
         int st = 0;
@@ -51,6 +63,9 @@ public class StringUtilsEx {
         return ((st > 0) || (len < text.length())) ? text.substring(st, len) : text;
     }
 
+    /**
+     * 默认字符 + 自定义字符的左向 trim
+     */
     public static String trimLeftPlus(String text, char... extras) {
         int len = text.length();
         int st = 0;
@@ -60,6 +75,9 @@ public class StringUtilsEx {
         return st > 0 ? text.substring(st, len) : text;
     }
 
+    /**
+     * 默认字符 + 自定义字符的右向 trim
+     */
     public static String trimRightPlus(String text, char... extras) {
         int len = text.length();
         while ((0 < len) && (text.charAt(len - 1) < ' ' || ArrayUtilsEx.indexOf(extras, text.charAt(len - 1)) >= 0)) {
@@ -68,4 +86,16 @@ public class StringUtilsEx {
         return len < text.length() ? text.substring(0, len) : text;
     }
 
+    /**
+     * 自定义多个字符的 indexOf
+     */
+    public static int indexOf(String text, int... chs) {
+        for (int i = 0; i < text.length(); i++) {
+            char c = text.charAt(i);
+            for (int ch : chs) {
+                if (c == ch) return i;
+            }
+        }
+        return -1;
+    }
 }
