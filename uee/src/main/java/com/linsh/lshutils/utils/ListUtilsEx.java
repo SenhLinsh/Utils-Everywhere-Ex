1db0e5d47ee5c1b9da9f3e857f7e2ed9fb88bfc7
Senh Linsh <lyuanxiang0812@126.com>
2021-09-21 22:57:01 +0800

    ✨ ListUtilsEx

--- uee/src/main/java/com/linsh/lshutils/utils/ListUtilsEx.java
+++ uee/src/main/java/com/linsh/lshutils/utils/ListUtilsEx.java
@@ -20,6 +20,16 @@ public class ListUtilsEx {
     }
 
     /**
+     * 获取集合长度（空指针判断）
+     *
+     * @param list 集合
+     * @return 集合长度，集合为 null 时返回 0
+     */
+    public static int size(List<?> list) {
+        return list == null ? 0 : list.size();
+    }
+
+    /**
      * 查找元素在集合中的索引
      *
      * @param list 集合
@@ -72,7 +82,8 @@ public class ListUtilsEx {
      * @return 转化后得到的 int 数组
      */
     public static <T> int[] toIntArray(List<T> list, Convertible<T, Integer> converter) {
-        if (converter == null) throw new IllegalArgumentException("converter can not be null");
+        if (converter == null)
+            throw new IllegalArgumentException("converter can not be null");
         if (list == null) return null;
         int[] array = new int[list.size()];
         for (int i = 0; i < array.length; i++) {
@@ -103,7 +114,8 @@ public class ListUtilsEx {
      * @return 转化后得到的 long 数组
      */
     public static <T> long[] toLongArray(List<T> list, Convertible<T, Long> converter) {
-        if (converter == null) throw new IllegalArgumentException("converter can not be null");
+        if (converter == null)
+            throw new IllegalArgumentException("converter can not be null");
         if (list == null) return null;
         long[] array = new long[list.size()];
         for (int i = 0; i < array.length; i++) {
@@ -134,7 +146,8 @@ public class ListUtilsEx {
      * @return 转化后得到的 float 数组
      */
     public static <T> float[] toFloatArray(List<T> list, Convertible<T, Float> converter) {
-        if (converter == null) throw new IllegalArgumentException("converter can not be null");
+        if (converter == null)
+            throw new IllegalArgumentException("converter can not be null");
         if (list == null) return null;
         float[] array = new float[list.size()];
         for (int i = 0; i < array.length; i++) {
@@ -165,7 +178,8 @@ public class ListUtilsEx {
      * @return 转化后得到的 double 数组
      */
     public static <T> double[] toDoubleArray(List<T> list, Convertible<T, Double> converter) {
-        if (converter == null) throw new IllegalArgumentException("converter can not be null");
+        if (converter == null)
+            throw new IllegalArgumentException("converter can not be null");
         if (list == null) return null;
         double[] array = new double[list.size()];
         for (int i = 0; i < array.length; i++) {
@@ -196,7 +210,8 @@ public class ListUtilsEx {
      * @return 转化后得到的 boolean 数组
      */
     public static <T> boolean[] toBooleanArray(List<T> list, Convertible<T, Boolean> converter) {
-        if (converter == null) throw new IllegalArgumentException("converter can not be null");
+        if (converter == null)
+            throw new IllegalArgumentException("converter can not be null");
         if (list == null) return null;
         boolean[] array = new boolean[list.size()];
         for (int i = 0; i < array.length; i++) {
@@ -227,7 +242,8 @@ public class ListUtilsEx {
      * @return 转化后得到的 char 数组
      */
     public static <T> char[] toCharArray(List<T> list, Convertible<T, Character> converter) {
-        if (converter == null) throw new IllegalArgumentException("converter can not be null");
+        if (converter == null)
+            throw new IllegalArgumentException("converter can not be null");
         if (list == null) return null;
         char[] array = new char[list.size()];
         for (int i = 0; i < array.length; i++) {
@@ -258,7 +274,8 @@ public class ListUtilsEx {
      * @return 转化后得到的 String 数组
      */
     public static <T> String[] toStringArray(List<T> list, Convertible<T, String> converter) {
-        if (converter == null) throw new IllegalArgumentException("converter can not be null");
+        if (converter == null)
+            throw new IllegalArgumentException("converter can not be null");
         if (list == null) return null;
         String[] array = new String[list.size()];
         for (int i = 0; i < array.length; i++) {
