197b322b8243416b8cdcb620a08f0b4b6fb47fa3
Senh Linsh <lyuanxiang0812@126.com>
2023-11-24 14:31:47 +0800

    ✨ 工具优化

--- uee/src/main/java/com/linsh/lshutils/utils/FileUtilsEx.java
+++ uee/src/main/java/com/linsh/lshutils/utils/FileUtilsEx.java
@@ -1,5 +1,9 @@
 package com.linsh.lshutils.utils;
 
+import androidx.annotation.Nullable;
+
+import java.io.File;
+
 /**
  * <pre>
  *    author : Senh Linsh
@@ -37,4 +41,43 @@ public class FileUtilsEx {
         return (attributes & 0x10) != 0;
     }
 
+    /**
+     * 在指定目录下查找指定名称（不带后缀）的文件
+     * <p>
+     *
+     * @param dir       指定目录
+     * @param name      文件名（不带后缀）
+     * @param postFixes 文件后缀，不传则表示任意后缀
+     * @return 找到的文件，未找到则返回 null
+     */
+    @Nullable
+    public static File findFile(File dir, String name, String... postFixes) {
+        if (dir != null && dir.isDirectory()) {
+            if (postFixes != null && postFixes.length > 0) {
+                for (String postFix : postFixes) {
+                    File file = new File(dir, name + postFix);
+                    if (file.exists()) {
+                        return file;
+                    }
+                }
+            } else {
+                File[] files = dir.listFiles();
+                if (files != null) {
+                    for (File file : files) {
+                        String fileName = file.getName();
+                        if (fileName.startsWith(name)) {
+                            if (fileName.length() == name.length()) {
+                                return file;
+                            }
+                            if (fileName.charAt(name.length()) == '.'
+                                    && fileName.indexOf('.', name.length() + 1) == -1) {
+                                return file;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return null;
+    }
 }
